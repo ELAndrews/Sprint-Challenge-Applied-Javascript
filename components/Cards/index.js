95e43e8b4df4f843a2ed201daae185eb85ae9f40
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-01 09:42:04 +0000

    Add function to programmatically create cards from data

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -19,8 +19,6 @@
 // Create a card for each of the articles and add the card to the DOM.
 
 
-const trial = axios.get('https://lambda-times-backend.herokuapp.com/articles')
-console.log(trial);
 
 function createCard (article) {
     const card = document.createElement('div');
@@ -29,21 +27,44 @@ function createCard (article) {
     const imgContainer = document.createElement('div');
     const img = document.createElement('img');
     const name = document.createElement('span');
-
+    
     card.classList.add('card');
     headline.classList.add('headline');
     author.classList.add('author');
     imgContainer.classList.add('img-container');
-
+    
     img.src = article.authorPhoto;
     headline.textContent = article.headline;
     name.textContent = `By ${article.authorName}`;
-
+    
     card.append(headline);
     card.append(author);
     author.append(imgContainer);
     author.append(name);
     imgContainer.append(img);
-
+    
     return card
 }
+// const trial = axios.get('https://lambda-times-backend.herokuapp.com/articles')
+// console.log(trial);
+
+const cardsContainer = document.querySelector('.cards-container');
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then((response) => {
+    const objPath = Object.assign(response.data.articles);
+    const newObjArr = Object.keys(objPath).map((key) =>{
+        return [key, objPath[key]];
+    })
+    newObjArr.forEach((arr) => {
+        arr[1].forEach((article) => {
+            cardsContainer.append(createCard(article));
+            console.log(`Card data collected for: ${article.headline}.`)
+        })
+    })
+    
+
+    })
+    .catch((error) => {
+        console.log(`There was an error collecting card data.`)
+    })
